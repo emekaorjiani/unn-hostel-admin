d6efbdbcf1cc0b342f412b4b589ab0dbba47402b
Younglad-Paul <borisadedayopaul@gmail.com>
2025-08-21 20:26:34 +0100

    fixed UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -403,7 +403,7 @@ export default function LandingPage() {
                   Restoring the dignity of man through excellence
                   <span className="block relative">
                     <motion.div 
-                      className="absolute -bottom-2 left-0 h-1 bg-yellow-400 transform -skew-x-12 w-full lg:w-89"
+                      className="absolute -bottom-2 left-0 h-1 bg-yellow-400 transform -skew-x-12 w-full lg:w-[60%]"
                       initial={{ scaleX: 0 }}
                       whileInView={{ scaleX: 1 }}
                       viewport={{ once: true }}
@@ -464,7 +464,7 @@ export default function LandingPage() {
                   <img
                     src="/VC.jpg"
                     alt="UNN Campus"
-                    className="w-full h-96 object-cover"
+                    className="w-full lg:h-96 object-cover"
                     // style={{
                     //   clipPath: 'polygon(0% 0%, 100% 0%, 85% 100%, 0% 100%)'
                     // }}
@@ -502,7 +502,7 @@ export default function LandingPage() {
                   <img
                     src="/unn13.png"
                     alt="UNN Campus Excellence"
-                    className="w-full h-96 object-cover"
+                    className="w-full lg:h-96 object-cover"
                     // style={{
                     //   clipPath: 'polygon(0% 0%, 100% 0%, 85% 100%, 0% 100%)'
                     // }}
